058a72a6e1ae16e74332d71da989e43f2aacff4b
gabrielaflores4 <gabriela.flores9@catolica.edu.sv>
2026-03-17 12:24:32 -0600

    Initial Commit

---
+++ Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by Gabriela Flores on 17/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculadora/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculadora
+//
+//  Created by Gabriela Flores on 17/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
